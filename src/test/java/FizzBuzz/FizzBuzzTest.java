792623ff98cddc36812ae7d50dea0150c4428478
Mariaregue-spec <mariareguecurso@gmail.com>
2026-03-03 14:21:10 +0100

    test test

---
+++ src/test/java/FizzBuzz/FizzBuzzTest.java
@@ -0,0 +1,17 @@
+package FizzBuzz;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FizzBuzzTest {
+    //Si el número es múltiplo de tres, devolverá la palabra "Fizz".
+    //Si el número es múltiplo de cinco, devolverá la palabra "Buzz".
+    //Si el número es múltiplo de tres y de cinco, devolverá la palabra "FizzBuzz".
+    //Si no coincide con las opciones de arriba retorna el número
+
+    @Test
+    void test(){
+        assertEquals(true, true);
+    }
+}
